621953c8e205b3b1dc3b4463c768ab669f1ce80b
Raq <Raq@LAPTOP-2>
2020-01-27 18:38:41 +1100

    Added  new fiellds to person dto

--- WebApiClient/Models/Dto/PersonDto.cs
+++ WebApiClient/Models/Dto/PersonDto.cs
@@ -7,6 +7,8 @@ namespace WebApiClient.Models.Dto
 {
     public class PersonDto
     {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
