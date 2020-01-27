5e072c4ec0503df4861cb5b98766f50d5cc56810
Raq <Raq@LAPTOP-2>
2020-01-27 18:13:55 +1100

    Added new field to person DTO

--- WebApiClient/Models/Dto/PersonDto.cs
+++ WebApiClient/Models/Dto/PersonDto.cs
@@ -7,7 +7,8 @@ namespace WebApiClient.Models.Dto
 {
     public class PersonDto
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
         public List<PetDto> pets { get; set; }
